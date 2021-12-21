4c393e2d6ab72748a50049847ae9e2f0d0b4e65c
Zhuolii <90219983+Zhuolii@users.noreply.github.com>
2021-12-21 01:25:59 +0100

    test

--- formulaire.php
+++ formulaire.php
@@ -1,4 +1,5 @@
 <?php
+/*
 if(isset($_POST['submit'])){
     $nom = $_POST['Nom'];
     $prenom = $_POST['prenom'];
@@ -30,7 +31,35 @@ if(isset($_POST['submit'])){
 
 }
 
+*/
 
+if(isset($_POST["submit"])){
+    $nom = $_POST['Nom'];
+    $prenom = $_POST['prenom'];
+    $email = $_POST['email'];
+    $telephone = $_POST['telephone'];
+    $objet = $_POST['objet'];
+    $message = $_POST['message'];
+
+
+    if(empty($nom) || empty($prenom) || empty($email) || empty($telephone) || empty($objet) || empty($message) ||){
+
+        header("location:contact.html?error");
+    }
+    else{
+
+        $to ="davidli271098@gmail.com";
+
+        if(mail($to, $objet, $message, $email)){
+            header("location:contact.html?success");
+        }
+    }
+
+
+}
+else{
+    header("location:contact.html");
+}
 
 
 
