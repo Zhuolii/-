f04d80aac57ffed9b8219a714d8a38ab76c9baa2
Zhuolii <90219983+Zhuolii@users.noreply.github.com>
2022-01-28 18:24:27 +0100

    update

---
+++ en/js/nav_background.js
@@ -0,0 +1,8 @@
+
+
+function toggle_background(){
+  var background = document.getElementById("nav_background")
+
+  background.classList.toggle("nav_background");
+  background.classList.toggle("sidebarList");
+}
---
+++ js/alert.js
@@ -0,0 +1,14 @@
+function hideAlert(){
+    
+ 
+  document.getElementById("alert_div").style.display ="none";
+  
+
+}
+function hideAlert2(){
+    
+ 
+  document.getElementById("alert_div2").style.display ="none";
+  
+
+}
