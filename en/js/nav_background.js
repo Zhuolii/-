d33601ae9d139a207d576339a62fcd702ec23192
Zhuolii <90219983+Zhuolii@users.noreply.github.com>
2022-01-28 22:26:41 +0100

    update images

--- en/js/nav_background.js
+++
@@ -1,8 +0,0 @@
-
-
-function toggle_background(){
-  var background = document.getElementById("nav_background")
-
-  background.classList.toggle("nav_background");
-  background.classList.toggle("sidebarList");
-}
