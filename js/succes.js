79ddb173ade072d495129223da5e482c7eec08c6
Zhuolii <90219983+Zhuolii@users.noreply.github.com>
2023-07-18 18:46:15 +0200

    update files

--- js/succes.js
+++ js/succes.js
@@ -7,3 +7,12 @@ var seconds = 5;
 				window.location.href = "https://www.chateaudemoliens.com/index.html";
 			}
 		}, 1000);
+        var seconds2 = 5;
+		var countdown2 = setInterval(function() {
+			seconds2--;
+			document.getElementById("countdown2").innerHTML = seconds2;
+			if (seconds2 == 0) {
+				clearInterval(countdown);
+				window.location.href = "https://www.chateaudemoliens.com/index.html";
+			}
+		}, 1000);
