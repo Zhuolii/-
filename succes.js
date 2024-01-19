c70215bd78cbf3c990659b44cc64759350696f66
Zhuolii <90219983+Zhuolii@users.noreply.github.com>
2024-01-19 19:49:38 +0100

    cleanup

--- succes.js
+++
@@ -1,9 +0,0 @@
-var seconds = 5;
-		var countdown = setInterval(function() {
-			seconds--;
-			document.getElementById("countdown").innerHTML = seconds;
-			if (seconds == 0) {
-				clearInterval(countdown);
-				window.location.href = "http://www.example.com";
-			}
-		}, 1000);
